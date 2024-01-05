6f8b7f4707f033d8dd0ba6dd6376a50d9cad58d4
udvikas <kvikashsharma09@gmail.com>
2024-01-05 15:36:58 +0530

    test.js file is added

---
+++ test.js
@@ -0,0 +1,22 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title><h1>Enter Message</h1></title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>"
+    );
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello Browser from Node JS Server</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+server.listen(3000);
