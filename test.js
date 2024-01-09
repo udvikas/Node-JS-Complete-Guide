03bd965950e02372644fd7ccc79bdbec9597854a
udvikas <kvikashsharma09@gmail.com>
2024-01-09 23:16:31 +0530

    router and filter functionality is added

--- test.js
+++ test.js
@@ -1,31 +1,21 @@
-const bodyParser = require("body-parser");
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const adminRoute = require("./routes/admin");
+const shopRoute = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use("/", (req, res, next) => {
 //   console.log("this is always runs");
 //   next();
 // });
+app.use("/admin", adminRoute);
+app.use(shopRoute);
 
-app.use("/add-product", (req, res, next) => {
-  // console.log("in another middle ware");
-  res.send(
-    '<form action="/product" method="POST"><input type= "text" name="title"><input type="number" name="size"><button type="submit">Add Product</button></form>'
-  );
-  // res.send("<h1>Welcome to Add Product Page</h1>");
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  console.log("in the middleware");
-  res.send("<h1>Welcome Express js Page</h1>");
+app.use((req, res, next) => {
+  res.status(404).send("<h1>Page Not Found</h1>");
 });
 
 app.listen(3000);
